e4e7bcdebe8dc4fd265545435f7c0d9fe906ea59
vdemel <vickijdemel@gmail.com>
2022-04-20 09:50:56 -0500

    set up fetch request to openweather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,7 @@
 var APIkey = "8cf87abd13d1d3d6082dce15342621f0";
+
+var getCityWeather = function() {
+    fetch("http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=8cf87abd13d1d3d6082dce15342621f0");
+};
+
+getCityWeather();
