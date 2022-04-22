507b35037de39b5b054fb01d246d56fd25af5c34
vdemel <vickijdemel@gmail.com>
2022-04-22 10:19:55 -0500

    updated form submission logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,63 @@
-var APIkey = "8cf87abd13d1d3d6082dce15342621f0";
-var userFormEl = document.querySelector("#user-form");
-var cityInputEl = document.querySelector("#cityName");
-var cityContainerEl = document.querySelector("#cities-container");
-var citySearchTerm = document.querySelector("city-search-term");
+var APIkey = '8cf87abd13d1d3d6082dce15342621f0';
+var userFormEl = document.querySelector('#user-form');
+var cityInputEl = document.querySelector('#cityName');
+var cityContainerEl = document.querySelector('#cities-container');
+var citySearchTerm = document.querySelector('#city-search-term');
 
-var formSubmitHandler = function(event) {
+var formSubmitHandler = function (event) {
     event.preventDefault();
-    // get value from input element
+    
     var cityName = cityInputEl.value.trim();
 
     if (cityName)  {
         getCityWeather(cityName);
 
         //clear old content
-        cityContainerEl.textContent = "";
-        cityInputEl.value = "";
+        cityContainerEl.textContent = '';
+        cityInputEl.value = '';
     } else {
-        alert("Please enter a valid city name");
+        alert('Please enter a valid city name');
     }
-} 
+};
 
-var getCityWeather = function(cityName) {
-    fetch("http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=8cf87abd13d1d3d6082dce15342621f0").then(function(response) {
-        response.json().then(function(data) {
-            displayWeather(data, cityName);
+// var buttonClickHandler = function (event) {
+//     var language = event.target.getAttribute('data-language');
+
+//     if (language) {
+//         getCityWeather(language);
+
+//         cityContainerEl.textContent = '';
+//     }
+// };
+
+var getCityWeather = function (cityName) {
+    fetch('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=8cf87abd13d1d3d6082dce15342621f0').then(function(response) {
+
+        if (response.ok) {
+            console.log(response);
+            response.json().then(function (data) {
+                console.log(data);
+                displayWeather(data, cityName);
         });
+    } else {
+        alert('Error: ' + response.statusText);
+    }
+    })
+    .catch(function (error) {
+        alert.apply('Unable to retrieve weather');
     });
-}
+};
 
-var displayWeather = function (cityName, SearchTerm) {
-    console.log(cityName);
-    console.log(citySearchTerm);
+var displayWeather = function (cityName, searchTerm) {
+
+    weatherSearchTerm.textContent = searchTerm;
+
+    
+    
 };
 
 // add event listener to form
-userFormEl.addEventListener("submit", formSubmitHandler);
+userFormEl.addEventListener('submit', formSubmitHandler);
 
-getCityWeather();
+
 
