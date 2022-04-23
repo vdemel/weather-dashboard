3e610ded82f6ba55da95634a5f0cf183245190b4
vdemel <vickijdemel@gmail.com>
2022-04-23 14:45:00 -0500

    got city to display and weather to load

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,28 +7,31 @@ var citySearchTerm = document.querySelector('#city-search-term');
 var formSubmitHandler = function (event) {
     event.preventDefault();
     console.log(event);
-};
+
     
     var cityName = cityInputEl.value.trim();
 
     if (cityName)  {
         getCityWeather(cityName);
 
-        //clear old content
-        cityContainerEl.textContent = '';
-        cityInputEl.value = '';
-    } else {
-        alert('Please enter a valid city name');
-    }
+//         //clear old content
+//         cityContainerEl.textContent = '';
+//         cityInputEl.value = '';
+//     } else {
+//         alert('Please enter a valid city name');
+    };
+}
 
-var getCityWeather = function (cityName) {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=APIkey') .then(function(response) { 
+var getCityWeather = function (weather) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Austin&appid=8cf87abd13d1d3d6082dce15342621f0";
+    fetch(apiUrl)
+     .then(function(response) { 
 
         if (response.ok) {
             console.log(response);
             response.json().then(function (data) {
                 console.log(data);
-                displayWeather(data, cityName);
+                displayWeather(data, weather);
         });
     } else {
         alert('Error: ' + response.statusText);
